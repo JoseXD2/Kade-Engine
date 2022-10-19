f3f3fe161c2a2d9cefb141927c2d921cbe74d4e9
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-10-19 11:33:51 -0300

    Tenso

--- source/LoadReplayState.hx
+++ source/LoadReplayState.hx
@@ -183,9 +183,9 @@ class LoadReplayState extends MusicBeatState
 
 				var songPath = "";
 
-				#if FEATURE_STEPMANIA
+				#if sys
 				if (PlayState.rep.replay.sm)
-					if (!FileSystem.exists(StringTools.replace(PlayState.rep.replay.chartPath, "converted.json", "")))
+					if (!Assets.exists(StringTools.replace(PlayState.rep.replay.chartPath, "converted.json", "")))
 					{
 						Application.current.window.alert("The SM file in this replay does not exist!", "SM Replays");
 						return;
@@ -201,7 +201,7 @@ class LoadReplayState extends MusicBeatState
 				#if FEATURE_STEPMANIA
 				if (PlayState.isSM)
 				{
-					songPath = File.getContent(PlayState.rep.replay.chartPath);
+					songPath = Assets.getText(PlayState.rep.replay.chartPath);
 					try
 					{
 						PlayState.sm = SMFile.loadFile(PlayState.pathToSm + "/" + StringTools.replace(PlayState.rep.replay.songName, " ", "_") + ".sm");
